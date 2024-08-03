c2ef7270ea4d12ead9079ce8df8a3cfa381a1887
Danish401 <alid13381@gmail.com>
2024-08-04 02:03:23 +0530

    first commit

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,6 +1,275 @@
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import AdminHomeView from "@/components/admin-view/home";
+// import AdminAboutView from "@/components/admin-view/about";
+// import AdminContactView from "@/components/admin-view/contact";
+// import AdminEducationView from "@/components/admin-view/education";
+// import AdminExperienceView from "@/components/admin-view/experience";
+// import AdminProjectView from "@/components/admin-view/project";
+// import { addData, getData, updateData, login } from "@/services";
+// import Login from "@/components/admin-view/login";
+
+// const initialHomeFormData = {
+//   heading: "",
+//   summary: "",
+// };
+
+// const initialAboutFormData = {
+//   aboutMe: "",
+//   noOfProjects: "",
+//   yearofexperience: "",
+//   noofclients: "",
+//   skills: "",
+// };
+
+// const initialEducationFormData = {
+//   degree: "",
+//   year: "",
+//   college: "",
+// };
+
+// const initialExperienceFormData = {
+//   position: "",
+//   company: "",
+//   duration: "",
+//   location: "",
+//   jobprofile: "",
+// };
+
+// const initialProjectFormData = {
+//   name: "",
+//   technologies: "",
+//   website: "",
+//   github: "",
+// };
+// const initialLoginFormData = {
+//   username: "",
+//   password: "",
+// };
+
+// export default function AdminView() {
+//   const [currentSelectedTab, setCurrentSelectedTab] = useState("home");
+//   const [homeViewFormData, setHomeViewFormData] = useState(initialHomeFormData);
+//   const [aboutViewFormData, setAboutViewFormData] =
+//     useState(initialAboutFormData);
+//   const [educationViewFormData, setEducationViewFormData] = useState(
+//     initialEducationFormData
+//   );
+//   const [experienceViewFormData, setExperienceViewFormData] = useState(
+//     initialExperienceFormData
+//   );
+//   const [projectViewFormData, setProjectViewFormData] = useState(
+//     initialProjectFormData
+//   );
+//   const [contacts, setContacts] = useState();
+//   const [allData, setAllData] = useState({});
+//   const [update, setUpdate] = useState(false);
+//   const [authUser, setAuthUser] = useState(false);
+//   const [loginFormData, setLoginFormData] = useState(initialLoginFormData);
+
+//   const menuItems = [
+//     {
+//       id: "home",
+//       label: "Home",
+//       Component: (
+//         <AdminHomeView
+//           formData={homeViewFormData}
+//           setFormData={setHomeViewFormData}
+//           handleSaveData={handleSaveData}
+//         />
+//       ),
+//     },
+//     {
+//       id: "about",
+//       label: "About",
+//       Component: (
+//         <AdminAboutView
+//           formData={aboutViewFormData}
+//           setFormData={setAboutViewFormData}
+//           handleSaveData={handleSaveData}
+//         />
+//       ),
+//     },
+//     {
+//       id: "experience",
+//       label: "Experience",
+//       Component: (
+//         <AdminExperienceView
+//           formData={experienceViewFormData}
+//           setFormData={setExperienceViewFormData}
+//           handleSaveData={handleSaveData}
+//           data={allData?.experience}
+//         />
+//       ),
+//     },
+//     {
+//       id: "education",
+//       label: "Education",
+//       Component: (
+//         <AdminEducationView
+//           formData={educationViewFormData}
+//           handleSaveData={handleSaveData}
+//           setFormData={setEducationViewFormData}
+//           data={allData?.education}
+//         />
+//       ),
+//     },
+//     {
+//       id: "project",
+//       label: "Project",
+//       Component: (
+//         <AdminProjectView
+//           formData={projectViewFormData}
+//           setFormData={setProjectViewFormData}
+//           handleSaveData={handleSaveData}
+//           data={allData?.project}
+//         />
+//       ),
+//     },
+//     {
+//       id: "contact",
+//       label: "Contact",
+//       Component: (
+//         <AdminContactView
+//           handleSaveData={handleSaveData}
+//           data={allData && allData?.contact}
+//         />
+//       ),
+//     },
+//   ];
+
+//   async function extractAllDatas() {
+//     console.log("Fetching data for tab:", currentSelectedTab);
+//     const response = await getData(currentSelectedTab);
+//     console.log("Response from API:", response);
+
+//     if (
+//       currentSelectedTab === "home" &&
+//       response &&
+//       response.data &&
+//       response.data.length
+//     ) {
+//       setHomeViewFormData(response && response.data[0]);
+//       setUpdate(true);
+//     }
+//     if (
+//       currentSelectedTab === "about" &&
+//       response &&
+//       response.data &&
+//       response.data.length
+//     ) {
+//       setAboutViewFormData(response && response.data[0]);
+//       setUpdate(true);
+//     }
+//     if (response?.success) {
+//       setAllData((prevData) => ({
+//         ...prevData,
+//         [currentSelectedTab]: response && response.data,
+//       }));
+//       console.log("Updated allData state:", allData);
+//     }
+//   }
+
+//   async function handleSaveData() {
+//     const dataMap = {
+//       home: homeViewFormData,
+//       about: aboutViewFormData,
+//       education: educationViewFormData,
+//       experience: experienceViewFormData,
+//       project: projectViewFormData,
+//     };
+//     const response = update
+//       ? await updateData(currentSelectedTab, dataMap[currentSelectedTab])
+//       : await addData(currentSelectedTab, dataMap[currentSelectedTab]);
+//     console.log("Response from saving data:", response);
+//     if (response.error) {
+//       console.error("Error saving data:", response.error);
+//       alert(`Error saving data: ${response.error}`);
+//       return;
+//     }
+//     if (response.success) {
+//       extractAllDatas(); // Update the data after saving
+//       resetFormDatas();
+//     }
+//   }
+
+//   useEffect(() => {
+//     extractAllDatas();
+//   }, [currentSelectedTab]);
+
+//   function resetFormDatas() {
+//     setHomeViewFormData(initialHomeFormData);
+//     setAboutViewFormData(initialAboutFormData);
+//     setExperienceViewFormData(initialExperienceFormData);
+//     setEducationViewFormData(initialEducationFormData);
+//     setProjectViewFormData(initialProjectFormData);
+//   }
+
+//   useEffect(() => {
+//     setAuthUser(JSON.parse(sessionStorage.getItem("authUser")));
+//   }, []);
+
+//   async function handleLogin() {
+//     const res = await login(loginFormData);
+//     console.log(res, "login");
+//     if (res?.success) {
+//       setAuthUser(true);
+//       sessionStorage.setItem("authUser", JSON.stringify(true));
+//     }
+//   }
+
+//   if (!authUser)
+//     return (
+//       <Login
+//         formData={loginFormData}
+//         handleLogin={handleLogin}
+//         setFormData={setLoginFormData}
+//       />
+//     );
+
+//   return (
+//     <div className="border-b border-gray-200">
+//       <nav className="-mb-0.5 flex justify-center space-x-6" role="tablist">
+//         {menuItems.map((item) => (
+//           <button
+//             key={item.id}
+//             type="button"
+//             className="p-4 font-bold text-xl text-black"
+//             onClick={() => {
+//               setCurrentSelectedTab(item.id);
+//               resetFormDatas(); // Call the reset function here if needed
+//               setUpdate(false);
+//             }}
+//           >
+//             {item.label}
+//           </button>
+//         ))}
+//         <button
+//           onClick={() => {
+//             setAuthUser(false);
+//             sessionStorage.removeItem("authUser");
+//           }}
+//           className="p-4 font-bold text-xl text-black"
+//         >
+//           Logout
+//         </button>
+//       </nav>
+//       <div className="mt-10 p-10">
+//         {menuItems.map(
+//           (item) =>
+//             item.id === currentSelectedTab && (
+//               <div key={item.id}>{item.Component}</div>
+//             )
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import AdminHomeView from "@/components/admin-view/home";
 import AdminAboutView from "@/components/admin-view/about";
 import AdminContactView from "@/components/admin-view/contact";
@@ -43,6 +312,7 @@ const initialProjectFormData = {
   website: "",
   github: "",
 };
+
 const initialLoginFormData = {
   username: "",
   password: "",
@@ -133,43 +403,32 @@ export default function AdminView() {
       Component: (
         <AdminContactView
           handleSaveData={handleSaveData}
-          data={allData && allData?.contact}
+          data={allData?.contact}
         />
       ),
     },
   ];
 
-  async function extractAllDatas() {
+  const extractAllDatas = useCallback(async () => {
     console.log("Fetching data for tab:", currentSelectedTab);
     const response = await getData(currentSelectedTab);
     console.log("Response from API:", response);
 
-    if (
-      currentSelectedTab === "home" &&
-      response &&
-      response.data &&
-      response.data.length
-    ) {
-      setHomeViewFormData(response && response.data[0]);
-      setUpdate(true);
-    }
-    if (
-      currentSelectedTab === "about" &&
-      response &&
-      response.data &&
-      response.data.length
-    ) {
-      setAboutViewFormData(response && response.data[0]);
-      setUpdate(true);
-    }
     if (response?.success) {
+      if (currentSelectedTab === "home" && response.data?.length) {
+        setHomeViewFormData(response.data[0]);
+        setUpdate(true);
+      }
+      if (currentSelectedTab === "about" && response.data?.length) {
+        setAboutViewFormData(response.data[0]);
+        setUpdate(true);
+      }
       setAllData((prevData) => ({
         ...prevData,
-        [currentSelectedTab]: response && response.data,
+        [currentSelectedTab]: response.data,
       }));
-      console.log("Updated allData state:", allData);
     }
-  }
+  }, [currentSelectedTab]);
 
   async function handleSaveData() {
     const dataMap = {
@@ -196,7 +455,7 @@ export default function AdminView() {
 
   useEffect(() => {
     extractAllDatas();
-  }, [currentSelectedTab]);
+  }, [currentSelectedTab, extractAllDatas]);
 
   function resetFormDatas() {
     setHomeViewFormData(initialHomeFormData);
@@ -266,3 +525,12 @@ export default function AdminView() {
     </div>
   );
 }
+
+
+echo "# port" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Danish401/port.git
+git push -u origin main
