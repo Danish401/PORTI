960e1824c138fc6c80a6d32025ac4815dc6f7f73
Danish401 <125354722+Danish401@users.noreply.github.com>
2024-08-04 03:48:37 +0530

    Update page.js

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -526,11 +526,3 @@ export default function AdminView() {
   );
 }
 
-
-echo "# port" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Danish401/port.git
-git push -u origin main
